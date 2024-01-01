f78719dc61b378d2b9067fb744a27e062311701a
vyas-puneet <puneet.vyas@apptadinc.com>
2024-01-01 16:59:27 +0530

    read directory in local system when you are create new folder

--- SpringWebSocket/src/main/java/com/socket/ctl/WebSocketCtl.java
+++ SpringWebSocket/src/main/java/com/socket/ctl/WebSocketCtl.java
@@ -3,12 +3,19 @@ package com.socket.ctl;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.socket.model.Greeting;
 import com.socket.model.HelloMessage;
 
 @Controller
 public class WebSocketCtl {
+
+	@GetMapping("/")
+	public String index(Model model) {
+		return "index.html";
+	}
 
 	@MessageMapping("/hello")
 	@SendTo("/topic/greetings")
--- SpringWebSocket/src/main/java/com/socket/service/DirectoryWatcherService.java
+++ SpringWebSocket/src/main/java/com/socket/service/DirectoryWatcherService.java
@@ -1,5 +1,56 @@
 package com.socket.service;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DirectoryWatcherService {
+
+	private final SimpMessagingTemplate simpMessagingTemplate;
+
+	public DirectoryWatcherService(SimpMessagingTemplate simpMessagingTemplate) {
+		this.simpMessagingTemplate = simpMessagingTemplate;
+		watchDirectory();
+	}
+
+	private void watchDirectory() {
+		String directoryPath = "D:\\\\Project Idea's";
+
+		try {
+			WatchService watchService = FileSystems.getDefault().newWatchService();
+			Path path = Paths.get(directoryPath);
+			path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
+
+			new Thread(() -> {
+				while (true) {
+					WatchKey key;
+					try {
+						key = watchService.take();
+					} catch (InterruptedException e) {
+						return;
+					}
+					for (WatchEvent<?> event : key.pollEvents()) {
+						if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+							String folderName = event.context().toString();
+							System.out.println("new folder created : " + folderName);
+							simpMessagingTemplate.convertAndSend("/topic/new-folder");
+						}
+					}
+					key.reset();
+				}
+			}).start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
